12f194e567552df7183f64d889c7a7c1d372e833
Askar <imangali950@gmail.com>
2024-02-03 00:48:58 +0600

    added EventDemo

---
+++ EventDemo/EventDemo/Program.cs
@@ -0,0 +1,99 @@
+﻿
+// ********** PRO TIP ***********
+// Use events to let others know about state change
+// or other significant events.
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        CollegeClassModel history = new CollegeClassModel("History 101", 3);
+        CollegeClassModel math = new CollegeClassModel("Math 201", 2);
+
+        history.EnrollmentFull += CollegeClass_EnrollmentFull;
+        history.EvenStudents += CollegeClass_EvenStudent;
+
+        history.SignUpStudent("Tim Corey").PrintToConsole();
+        history.SignUpStudent("Sue Smith").PrintToConsole();
+        history.SignUpStudent("Jonm Stones").PrintToConsole();
+        history.SignUpStudent("Kary Petty").PrintToConsole();
+        history.SignUpStudent("Sanda Shmeichel").PrintToConsole();
+
+        math.EnrollmentFull += CollegeClass_EnrollmentFull;
+        math.EvenStudents += CollegeClass_EvenStudent;
+
+
+        math.SignUpStudent("Sue Smith").PrintToConsole();
+        math.SignUpStudent("Jonm Stones").PrintToConsole();
+        math.SignUpStudent("Kary Petty").PrintToConsole();
+
+    }
+
+    private static void CollegeClass_EvenStudent(object sender, string e)
+    {
+        System.Console.WriteLine();
+        System.Console.WriteLine(e);
+        System.Console.WriteLine();
+    }
+
+    private static void CollegeClass_EnrollmentFull(object sender, string e)
+    {
+        CollegeClassModel model = (CollegeClassModel)sender;
+
+        System.Console.WriteLine();
+        System.Console.WriteLine($"{model.CourseTitle}: full");
+        System.Console.WriteLine();
+    }
+}
+
+public static class ConsoleHelper
+{
+    public static void PrintToConsole(this string message)
+    {
+        System.Console.WriteLine(message);
+    }
+}
+
+public class CollegeClassModel
+{
+
+    public event EventHandler<string> EnrollmentFull;
+    public event EventHandler<string> EvenStudents;
+    private List<string> enrolledStudents = new List<string>();
+    private List<string> waitingStudents = new List<string>();
+
+    public string CourseTitle { get; private set; }
+    public int MaximumStudents { get; private set; }
+
+    public CollegeClassModel(string title, int maximumStudents)
+    {
+        CourseTitle = title;
+        MaximumStudents = maximumStudents;
+    }
+
+    public string SignUpStudent(string studentName)
+    {
+        string output = "";
+        if (enrolledStudents.Count < MaximumStudents)
+        {
+            enrolledStudents.Add(studentName);
+            output = $"{studentName} was enrolled in {CourseTitle}";
+            if (enrolledStudents.Count == MaximumStudents)
+            {
+                EnrollmentFull?.Invoke(this, $"{CourseTitle} is full");
+            }
+
+            if (enrolledStudents.Count % 2 == 0)
+            {
+                EvenStudents?.Invoke(this, $"{studentName} is even student");
+            }
+        }
+        else
+        {
+            waitingStudents.Add(studentName);
+            output = $"{studentName} was added in waiting list {CourseTitle}";
+
+        }
+        return output;
+    }
+}
---
+++ GenericsDemo/GenereicsDemo/Program.cs
@@ -0,0 +1,86 @@
+﻿
+// ******** PRO TIP *********
+// The standard generic variable is T
+// but you can choose any letter you want.
+
+// string result = FizzBuzz<string>("ABC");
+// System.Console.WriteLine($"ABC: {result}");
+
+// result = FizzBuzz<int>(12345);
+// System.Console.WriteLine($"12345: {result}");
+
+// result = FizzBuzz<bool>(true);
+// System.Console.WriteLine($"true: {result}");
+
+
+// result = FizzBuzz<PersonModel>(new PersonModel());
+// System.Console.WriteLine($"PersonModel: {result}");
+
+
+GenericHelper<PersonModel> personHelper = new GenericHelper<PersonModel>();
+personHelper.CheckItemAndAdd(new PersonModel { FirstName = "Tim", HasError = true });
+personHelper.CheckItemAndAdd(new PersonModel { FirstName = "Bob", HasError = false });
+
+
+foreach (var item in personHelper.RejectedItems)
+{
+    System.Console.WriteLine($"{item.FirstName} {item.LastName} was rejected");
+}
+
+foreach (var item in personHelper.Items)
+{
+    System.Console.WriteLine($"{item.FirstName} {item.LastName} was good");
+}
+
+
+string FizzBuzz<T>(T item)
+{
+    string output = "";
+    int itemLength = item.ToString().Length;
+    if (itemLength % 3 == 0)
+    {
+        output += "Fizz";
+    }
+    if (itemLength % 5 == 0)
+    {
+        output += "Buzz";
+    }
+    return output;
+}
+
+public class GenericHelper<T> where T : IErrorCheck
+{
+    public List<T> Items { get; set; } = new List<T>();
+    public List<T> RejectedItems { get; set; } = new List<T>();
+
+    public void CheckItemAndAdd(T item)
+    {
+        if (!item.HasError)
+        {
+            Items.Add(item);
+        }
+        else
+        {
+            RejectedItems.Add(item);
+        }
+    }
+}
+
+public interface IErrorCheck
+{
+    bool HasError { get; set; }
+}
+
+public class Car : IErrorCheck
+{
+    public string Manufacturer { get; set; }
+    public int YearManufactured { get; set; }
+    public bool HasError { get; set; }
+}
+
+public class PersonModel : IErrorCheck
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool HasError { get; set; }
+}
