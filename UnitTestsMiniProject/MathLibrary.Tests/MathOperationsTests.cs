04cd01b9af7cd92fb662f716ff64566de73fd89c
Askar <imangali950@gmail.com>
2024-02-04 01:26:50 +0600

    added UnitTestsMiniProject

---
+++ UnitTestsMiniProject/MathLibrary.Tests/MathOperationsTests.cs
@@ -0,0 +1,58 @@
+﻿namespace MathLibrary.Tests;
+
+public class MathOperationsTests
+{
+
+    [Theory]
+    [InlineData(10, 5, 15)]
+    [InlineData(30, 4, 34)]
+    [InlineData(0, -5, -5)]
+    public void AddShouldReturnExpectedResult(double x, double y, double expected)
+    {
+        MathOperations mathOperations = new MathOperations();
+
+        double actual = mathOperations.Add(x, y);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(10, 5, 5)]
+    [InlineData(30, 4, 26)]
+    [InlineData(0, -5, 5)]
+    public void SubstractShouldReturnExpectedResult(double x, double y, double expected)
+    {
+        MathOperations mathOperations = new MathOperations();
+
+        double actual = mathOperations.Substract(x, y);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(10, 5, 50)]
+    [InlineData(30, 4, 120)]
+    [InlineData(0, -5, 0)]
+    public void MultiplyShouldReturnExpectedResult(double x, double y, double expected)
+    {
+        MathOperations mathOperations = new MathOperations();
+
+        double actual = mathOperations.Multiply(x, y);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(10, 5, 2)]
+    [InlineData(30, 4, 7.5)]
+    [InlineData(0, -5, 0)]
+    [InlineData(8, 0, 0)]
+    public void DivideShouldReturnExpectedResult(double x, double y, double expected)
+    {
+        MathOperations mathOperations = new MathOperations();
+
+        double actual = mathOperations.Divide(x, y);
+
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ UnitTestsMiniProject/MathLibrary/MathOperations.cs
@@ -0,0 +1,36 @@
+﻿namespace MathLibrary;
+
+
+// ********* PRO TIP *************
+// Test more than just the perfect case.
+// Test all of the imperfect cases most of all.
+
+public class MathOperations
+{
+    public double Add(double x, double y)
+    {
+        return x + y;
+    }
+
+    public double Substract(double x, double y)
+    {
+        return x - y;
+    }
+
+    public double Multiply(double x, double y)
+    {
+        return x * y;
+    }
+
+    public double Divide(double x, double y)
+    {
+        double output = 0;
+
+        if (y != 0)
+        {
+            output = x / y;
+        }
+        return output;
+    }
+
+}
