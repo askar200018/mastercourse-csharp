824ca7ce10df7b1f8ce2f1d71ca95f47126c475a
Askar <imangali950@gmail.com>
2024-02-14 23:59:42 +0600

    added MessageWallApi

---
+++ MessageWallApi/MessageWallApi/Controllers/MessageWallController.cs
@@ -0,0 +1,30 @@
+﻿using MessageWallApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessageWallApi;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MessageWallController : ControllerBase
+{
+    private readonly ILogger<MessageWallController> _logger;
+
+    public MessageWallController(ILogger<MessageWallController> logger)
+    {
+        _logger = logger;
+    }
+
+    // GET: api/MessageWall
+    [HttpGet(Name = "GetMessages")]
+    public string Get(string firstName = "", string lastName = "")
+    {
+        return $"Hello, {firstName} {lastName}!";
+    }
+
+    // POST: api/MessageWall
+    [HttpPost(Name = "PostMessage")]
+    public void Post([FromBody] MessageModel value)
+    {
+        _logger.LogInformation($"Message received: {value.Message}");
+    }
+}
---
+++ MessageWallApi/MessageWallApi/Models/MessageModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MessageWallApi.Models;
+
+public class MessageModel
+{
+    public int Id { get; set; }
+    public string Message { get; set; }
+}
