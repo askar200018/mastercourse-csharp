9d15dfdefbffb32f951634e1826893bffa62fa48
Askar <imangali950@gmail.com>
2024-02-18 00:32:20 +0600

    added BlazorServerMiniProject

---
+++ BlazorServerMiniProject/BlazorServerMiniProject/Models/AddressModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerMiniProject.Models;
+
+public class AddressModel
+{
+    [Required]
+    public string City { get; set; }
+    [Required]
+    public string Street { get; set; }
+    [Required]
+    public string ZipCode { get; set; }
+}
