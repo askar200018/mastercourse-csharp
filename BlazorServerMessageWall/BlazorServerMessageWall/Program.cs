2d57cd600d8a26e398fa6c0694ed5609ca176383
Askar <imangali950@gmail.com>
2024-02-17 23:55:55 +0600

    added BlazorServerMessageWall

---
+++ BlazorServerMessageWall/BlazorServerMessageWall/Program.cs
@@ -0,0 +1,33 @@
+using BlazorServerMessageWall.Components;
+
+
+// **** PRO TIP ****
+// Blazor Server is a part of ASP.NET Core,
+// which means you can use other projec types
+// with it such as MVC or Razor Pages.
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ CoreApiMiniProject/CoreApiMiniProject/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// ****** PRO TIP ******
+// If you are going to learn one web application type,
+// this is the one to learn first and best.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
